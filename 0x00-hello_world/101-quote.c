c1299f6820a5d7f3e862306750cd00cd84f66f55
Ukeremi <ukpono9@gmail.com>
2023-02-16 10:39:23 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A c program that prints a line to the standard erro
+ * main - A c program that prints a line to thei standard erro
  * Return: 1 (success)
  */
 int main(void)
 {
-	write("and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fwrite("and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
