5ac74d3a830aa7678320adc1375a023f6c179728
Ukeremi <ukpono9@gmail.com>
2023-02-16 13:36:03 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
-
+#include <stdio.h>
 /**
- * main - A c program that prints a line to thei standard erro
+ * main - A c program that prints a line to the standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"
-			- Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
