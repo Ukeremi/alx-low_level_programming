e73156a3f2b191cdc9504477b13dbfeb1869e691
Ukeremi <ukpono9@gmail.com>
2023-02-17 05:35:42 -0800

    Game of alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main -  Entry point
+ *
+ * Description: Prints the alphabet in lowercase.
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
