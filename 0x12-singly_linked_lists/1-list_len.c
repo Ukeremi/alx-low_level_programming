08af977ca431e9f17610959955b87f372f79f682
Ukeremi <64284814+Ukeremi@users.noreply.github.com>
2023-04-05 21:50:59 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,20 +2,20 @@
 
 /**
  * list_len - Finds the number of elements in
- *            a linked list_t list.
+ *            a list.
  * @h: The linked list_t list.
  *
  * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t elem = 0;
 
 	while (h)
 	{
-		elements++;
+		elem++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (elem);
 }
