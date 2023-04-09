5f1e4533e859053b7c9c79632a7ec31c463e69d7
Ukeremi <ukpono9@gmail.com>
2023-04-09 13:49:16 -0700

    2-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
+#include "main.h"
+#include <sys/types.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
-* main - this is a  program that copies the content of a file to another file
-* @argc: number of argument
-* @argv: string argument
-* Return: 0
-*/
-
+ * main - This program is to copy the content of a file to another file.
+ * @argc: number of arguments.
+ * @argv: arguments
+ * Return: 0 on sucess
+ */
 int main(int argc, char *argv[])
 {
-int file_from, file_to;
-int num_one = 1024, num_two = 0;
-char buf[1024];
+	int uk, uk2, w;
+	ssize_t sz = 1024;
+	char buf[1024];
 
-if (argc != 3)
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-file_from = open(argv[1], O_RDONLY);
-
-if (file_from == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-}
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-	| S_IRGRP | S_IWGRP | S_IROTH);
-
-if (file_to == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	close(file_from), exit(99);
-}
-while (num_one == 1024)
-{
-	num_one = read(file_from, buf, 1024);
-	if (num_one == -1)
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
+	uk = open(argv[1], O_RDONLY);
+	if (uk == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	num_two = write(file_to, buf, num1);
-	if (num_two < num_one)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-}
-if (close(file_from) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-if (close(file_to) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-
-return (0);
+		exit(98); }
+	uk2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	if (uk2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
+	while (sz == 1024)
+	{
+		sz = read(uk, buf, 1024);
+		if (sz == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
+		w = write(uk2, buf, sz);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99); }}
+	if (close(uk) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100); }
+	if (close(uk2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100); }
+	return (0);
 }
