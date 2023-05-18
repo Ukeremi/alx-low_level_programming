bf69e13eb825978858706804ec074b62651f29b4
Ukeremi <ukpono9@gmail.com>
2023-05-18 05:47:57 -0700

    print number of element

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -13,7 +13,7 @@ size_t dlistint_len(const dlistint_t *h)
 	unsigned int count = 0;
 	const dlistint_t *list;
 
-	list = maloc(sizeof(dlistint_t));
+	list = malloc(sizeof(dlistint_t));
 	list = h;
 
 	if (list == NULL)
